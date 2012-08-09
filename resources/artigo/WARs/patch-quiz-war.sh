8f1d2f5e342b54cdb68929283114e3805343e8ce
Fabian Kneißl <fabian.kneissl@ifi.lmu.de>
2012-08-09 11:30:49 +0200

    playn-quiz updated

--- resources/artigo/WARs/patch-quiz-war.sh
+++ resources/artigo/WARs/patch-quiz-war.sh
@@ -4,13 +4,13 @@
 
 [ -e /resources/artigo/WARs/playn-quiz-html-1.0.war ] || (echo "Could not find file, run from project root"; exit 1)
 
-cd tmp && rm -rf *
-jar xf ../resources/artigo/WARs/playn-quiz-html-1.0.war
+mkdir -p tmp && cd tmp && rm -rf * && \
+jar xf ../resources/artigo/WARs/playn-quiz-html-1.0.war && \
 cat > WEB-INF/jboss-web.xml <<EOF
 <jboss-web>
         <context-root>/playn-quiz</context-root>
         <virtual-host>artigo</virtual-host>
 </jboss-web>
 EOF
-jar cf ../resources/artigo/WARs/playn-quiz-html-1.0.war *
+$! && jar cf ../resources/artigo/WARs/playn-quiz-html-1.0.war *
 
