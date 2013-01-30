82bc62eecfd8b3665759ce405b558f51d8fb4bb9
Fabian Kneißl <fabian.kneissl@ifi.lmu.de>
2013-01-30 18:26:20 +0100

    Changed License header

--- src/hot/gwap/action/TaggingBean.java
+++ src/hot/gwap/action/TaggingBean.java
@@ -1,9 +1,23 @@
 /*
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * This file is part of gwap
  *
- * Copyright (c) 2009-11, Lehrstuhl PMS (http://www.pms.ifi.lmu.de/)
- * All rights reserved.
+ * Copyright (C) 2013
+ * Project play4science
+ * Lehr- und Forschungseinheit für Programmier- und Modellierungssprachen
+ * Ludwig-Maximilians-Universität München
  * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package gwap.action;
