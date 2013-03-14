ed8a2cd008d26c7bdd9c04841e566bbeba38b0e0
Christoph Wieser <christoph.wieser@ifi.lmu.de>
2013-03-14 16:55:37 +0100

    Superclass for Rating

--- src/main/gwap/model/Rating.java
+++ src/main/gwap/model/Rating.java
@@ -22,28 +22,58 @@
 
 package gwap.model;
 
+/**
+ * Generic Rating
+ * 
+ * @author wieser, maders
+ */
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 
-import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Scope;
-
 @Entity
-@Name("gameSessionRating")
-@Scope(ScopeType.CONVERSATION)
-public class GameSessionRating extends Rating implements Serializable {
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
+public abstract class Rating implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	@ManyToOne	private GameSession gameSession;
-	public GameSession getGameSession() {
-		return gameSession;
+	@Id
+	@GeneratedValue
+	private Long id;
+	@ManyToOne
+	private Person person;
+	private Long rating = 0L;
+
+	public Rating() {
+		super();
 	}
 
-	public void setGameSession(GameSession gameSession) {
-		this.gameSession = gameSession;
+	public Long getId() {
+		return id;
 	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	public Long getRating() {
+		return rating;
+	}
+
+	public void setRating(Long rating) {
+		this.rating = rating;
+	}
+
 }
